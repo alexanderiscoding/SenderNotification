f947a7df69a8eff23219299583abe8a123839765
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2023-01-07 21:27:06 -0300

    padronization

--- pages/api/fcm/topic.js
+++ pages/api/fcm/topic.js
@@ -3,38 +3,52 @@ export default function handler(req, res) {
   if (req.headers.authorization != process.env.TOKEN) {
     return res.status(401).json("Invalid Authentication Credentials");
   }
-  if ([req.body.coduser, req.body.registrationToken, req.body.topic, req.body.activate].includes(undefined)) {
+  if ([req.body.coduser, req.body.topic, req.body.activate].includes(undefined)) {
     return res.status(406).json("Missing Information");
   }
-  return fetch(process.env.CLOUD_HOST + '/api/messaging/topic', {
+  return fetch(process.env.CLOUD_HOST + '/api/database/read', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
       'Authorization': process.env.CLOUD_TOKEN
     },
     body: JSON.stringify({
-      registrationTokens: [req.body.registrationToken],
-      topic: req.body.topic,
-      activate: req.body.activate
+      id: 'SenderNotification/' + req.body.coduser
     })
-  }).then(function () {
-    return fetch(process.env.CLOUD_HOST + '/api/database/update', {
+  }).then(async function (response) {
+    const data = await response.json();
+    return fetch(process.env.CLOUD_HOST + '/api/messaging/topic', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': process.env.CLOUD_TOKEN
       },
       body: JSON.stringify({
-        name: "SenderNotification",
-        id: req.body.coduser,
-        column: {
-          topic: {
-            [req.body.topic]: req.body.activate
-          }
-        }
+        registrationTokens: [data.registrationToken],
+        topic: req.body.topic,
+        activate: req.body.activate
       })
-    }).then(function () {
-      res.status(200).json('User device registred in topic.');
+    }).then(async function () {
+      return fetch(process.env.CLOUD_HOST + '/api/database/update', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': process.env.CLOUD_TOKEN
+        },
+        body: JSON.stringify({
+          name: "SenderNotification",
+          id: req.body.coduser,
+          column: {
+            topic: {
+              [req.body.topic]: req.body.activate
+            }
+          }
+        })
+      }).then(function () {
+        res.status(200).json('User device changed in topic.');
+      }).catch(function (error) {
+        res.status(500).json(error);
+      });
     }).catch(function (error) {
       res.status(500).json(error);
     });
--- pages/api/fcm/update.js
+++ pages/api/fcm/update.js
@@ -21,7 +21,7 @@ export default function handler(req, res) {
       }
     })
   }).then(function () {
-    res.status(200).json('User device salved.');
+    res.status(200).json('User device updated.');
   }).catch(function (error) {
     res.status(500).json(error);
   });
